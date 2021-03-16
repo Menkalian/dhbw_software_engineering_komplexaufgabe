ae625a2561d46ae44e0ed235ebe589c9e009ff11
Kilian Krampf <kilian@krampf.de>
2021-03-16 20:40:13 +0100

    Implement SortingTrack and subclasses

--- implementation/src/main/java/packagecenter/parts/sortingsystem/tracks/sorting/SortingTrack.java
+++ implementation/src/main/java/packagecenter/parts/sortingsystem/tracks/sorting/SortingTrack.java
@@ -1,15 +1,23 @@
 package packagecenter.parts.sortingsystem.tracks.sorting;
 
-import packagecenter.incomming.*;
 import packagecenter.incomming.Package;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public abstract class SortingTrack {
-    private SortingTrack successor;
-    private List<Package> packages;
-    private IScanner scanner;
-    private List<Package> dangerousPackages;
+    private final SortingTrack successor;
+    private final List<Package> packages;
+    private final IScanner scanner;
+    private final List<Package> dangerousPackages;
+
+    public SortingTrack(SortingTrack successor) {
+        this.successor = successor;
+
+        scanner = new Scanner();
+        packages = new LinkedList<>();
+        dangerousPackages = new LinkedList<>();
+    }
 
     public List<Package> getPackages() {
         return this.packages;
@@ -23,28 +31,19 @@ public abstract class SortingTrack {
         return this.dangerousPackages;
     }
 
-    /**
-     * 
-     * @param pkg
-     */
-    public abstract void doesAccept(Package pkg);
+    public abstract boolean doesAccept(Package pkg);
 
-    /**
-     * 
-     * @param pkg
-     */
     public void accept(Package pkg) {
-        // TODO - implement SortingTrack.accept @Löh
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * 
-     * @param successor
-     */
-    public SortingTrack(SortingTrack successor) {
-        // TODO - implement SortingTrack.SortingTrack @Löh
-        throw new UnsupportedOperationException();
+        if (doesAccept(pkg)) {
+            if(scanner.scan(pkg)){
+                dangerousPackages.add(pkg);
+                System.out.println("Package " + pkg.getId() + " sorted out and destroyed!");
+            } else {
+                packages.add(pkg);
+            }
+        } else {
+            successor.accept(pkg);
+        }
     }
 
 }
--- implementation/src/main/java/packagecenter/parts/sortingsystem/tracks/sorting/SortingTrackExpress.java
+++ implementation/src/main/java/packagecenter/parts/sortingsystem/tracks/sorting/SortingTrackExpress.java
@@ -1,26 +1,16 @@
 package packagecenter.parts.sortingsystem.tracks.sorting;
 
 import packagecenter.incomming.Package;
+import packagecenter.incomming.PackageType;
 
 public class SortingTrackExpress extends SortingTrack {
 
-    /**
-     * 
-     * @param successor
-     */
     public SortingTrackExpress(SortingTrack successor) {
         super(successor);
-        // TODO - implement SortingTrackExpress.SortingTrackExpress @Löh
-        throw new UnsupportedOperationException();
     }
 
-    /**
-     * 
-     * @param pkg
-     */
-    public void doesAccept(Package pkg) {
-        // TODO - implement SortingTrackExpress.doesAccept @Löh
-        throw new UnsupportedOperationException();
+    public boolean doesAccept(Package pkg) {
+        return pkg.getType() == PackageType.EXPRESS;
     }
 
 }
