5973bbe2c4735129ea5beee95cac2b89a46618f8
Kilian Krampf <kilian@krampf.de>
2021-03-16 21:37:04 +0100

    Implement Rabin_Karp Algorithm

--- implementation/components/rabin_karp/src/main/java/ScannerAlgorithm.java
+++ implementation/components/rabin_karp/src/main/java/ScannerAlgorithm.java
@@ -1,4 +1,9 @@
+import java.util.function.Function;
+
 public class ScannerAlgorithm {
+    private static final int d = 256; // Size of alphabet = 256
+    private static final int q = 101; // some prime
+
     // static instance
     private static ScannerAlgorithm instance = new ScannerAlgorithm();
     // port
@@ -8,14 +13,58 @@ public class ScannerAlgorithm {
         port = new Port();
     }
 
-    // TODO
-    public String innerGetVersion(){return "";}
-    public int innerScan(String toSearch, char[][][] toScan){return -1;}
+    public String innerGetVersion() {
+        return "rk_01";
+    }
+
+    public int innerScan(String pattern, char[][][] text) {
+        int m = pattern.length();
+        int n = text.length * text[0].length * text[0][0].length;
+
+        int x = text[0][0].length;
+        int y = x * text[0].length;
+        Function<Integer, Character> txtAt = i -> text[i / y][i / x][i % x];
+
+        int p = 0; // Hash value of the pattern
+        int t = 0; // Hash value of the  current textwindow
+        int h = 1;
+
+        for (int i = 0 ; i < m ; i++) {
+            p = (d * p + pattern.charAt(i)) % q;
+            t = (d * t + text[i / y][i / x][i % x]) % q;
+        }
+
+        // Slide over text one by one
+        for (int i = 0 ; i < n - m ; i++) {
+            if (p == t) {
+                int j;
+                for (j = 0; j < m ; j++) {
+                    if (txtAt.apply(i + j) != pattern.charAt(j))
+                        break;
+                }
+
+                if (j == m)
+                    return i;
+            }
+
+            // Calculate next window by removing the first char and adding the next
+            if (i < n - m) {
+                t = (d * (t - txtAt.apply(i) * h) + txtAt.apply(i + m)) % q;
+            }
+        }
+
+        return -1;
+    }
 
     public class Port implements IScannerAlgorithm {
         @Override
-        public String getVersion(){return innerGetVersion();}
+        public String getVersion() {
+            return innerGetVersion();
+        }
+
         @Override
-        public int scan(String toSearch, char[][][] toScan){return innerScan(toSearch, toScan);}
+        public int scan(String toSearch, char[][][] toScan) {
+            return innerScan(toSearch, toScan);
+        }
     }
 }
